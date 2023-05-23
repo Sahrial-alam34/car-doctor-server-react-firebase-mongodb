5b4529984fe5c77dead2b6396fccff3708802ad8
Sahrial-alam34 <sunnykcp01@gmail.com>
2023-05-23 11:16:56 +0600

    search, sort

--- index.js
+++ index.js
@@ -24,20 +24,20 @@ const client = new MongoClient(uri, {
     }
 });
 
-const verifyJWT = (req, res, next)=>{
+const verifyJWT = (req, res, next) => {
     console.log('hitting verify JWT');
     console.log(req.headers.authorization);
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error:true, message: 'unauthorized access'})
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
 
     const token = authorization.split(' ')[1];
     console.log('token inside verify JWT', token);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if(err){
-            return res.status(403).send({error: true, message:'unauthorized access'})
-        } 
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ error: true, message: 'unauthorized access' })
+        }
         req.decoded = decoded;
         next();
     })
@@ -53,31 +53,46 @@ async function run() {
         const bookingCollection = client.db('carDoctor').collection('bookings')
 
         // jwt
-        app.post('/jwt', (req, res)=>{
+        app.post('/jwt', (req, res) => {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '23h'
             });
             console.log(token)
-            res.send({token});
+            res.send({ token });
         })
 
-        app.get('/services', async(req, res)=>{
-            const cursor = serviceCollection.find();
+        app.get('/services', async (req, res) => {
+            const sort = req.query.sort;
+            const search = req.query.search;
+            console.log(search);
+            // const query = {};
+            // const query = { price: {$gt:50, $lte:150}};
+
+            const query = {title: {$regex: search, $options: 'i'}}
+
+            const options = {
+                // sort returned documents in descending order by title (A->Z)
+                sort: { 
+                    "price": sort === 'ascending' ? 1 : -1 
+                }
+             
+            };
+            const cursor = serviceCollection.find(query, options);
             const result = await cursor.toArray();
             res.send(result)
         })
 
-        app.get('/services/:id', async(req, res)=>{
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
-            const query = { _id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
 
             const options = {
-          
+
                 // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, price: 1 , service_id: 1 , img:1},
-              };
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
+            };
 
             const result = await serviceCollection.findOne(query, options);
             res.send(result);
@@ -85,52 +100,52 @@ async function run() {
 
         // bookings routes
 
-        app.get('/bookings',verifyJWT, async( req, res)=>{
-           // console.log(req.query.email);
+        app.get('/bookings', verifyJWT, async (req, res) => {
+            // console.log(req.query.email);
             //console.log(req.headers);
             //console.log(req.headers.authorization);
             const decoded = req.decoded;
             console.log('came back after verify', decoded)
 
             //verify  email
-            if(decoded.email !== req.query.email){
-                return res.status(403).send({error:1, message:'forbidden access'})
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
             }
-             let query = {};
-            if(req.query?.email){
-                query = {email: req.query.email}
+            let query = {};
+            if (req.query?.email) {
+                query = { email: req.query.email }
             }
             const result = await bookingCollection.find(query).toArray();
             res.send(result);
         })
 
-        app.post('/bookings',async(req, res)=>{
-            const booking  = req.body;
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
             console.log(booking);
             const result = await bookingCollection.insertOne(booking);
             res.send(result)
 
         });
 
-        app.patch('/bookings/:id', async(req, res)=>{
+        app.patch('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const filter = {_id: new ObjectId(id)};
+            const filter = { _id: new ObjectId(id) };
             const updatedBooking = req.body;
             console.log(updatedBooking);
             const updateDoc = {
                 $set: {
-                  status: updatedBooking.status
+                    status: updatedBooking.status
                 },
-              };
-              const result = await bookingCollection.updateOne(filter, updateDoc);
-              res.send(result)
-          
+            };
+            const result = await bookingCollection.updateOne(filter, updateDoc);
+            res.send(result)
+
 
         })
 
-        app.delete('/bookings/:id', async(req, res)=>{
+        app.delete('/bookings/:id', async (req, res) => {
             const id = req.params.id;
-            const query = {_id: new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const result = await bookingCollection.deleteOne(query);
             res.send(result);
 
@@ -142,7 +157,7 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-      //  await client.close();
+        //  await client.close();
     }
 }
 run().catch(console.dir);
